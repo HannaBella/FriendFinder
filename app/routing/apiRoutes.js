e10f90698b16bc2f4fc65fb9b5f159f1bfae8381
Mitu Fikir <hannatefera2@gmail.com>
2019-09-21 18:38:19 -0700

    Added routes and form validation

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,28 +9,29 @@ module.exports = function(app) {
     app.post("/api/friends", function(req, res) {
 
         var bestMatch = {
-            name: "",
-            photo: "",
-            scoreDifference: 1000
-        }
-        var userScoreSum = 0;
+                name: "",
+                photo: "",
+                scoreDifference: 100
+            }
+            //var userScoreSum = 0;
         var userData = req.body;
         var userScores = userData.scores;
+        var totalDifference = 0;
 
-        for (var i = 0; i < userScores.length; i++) {
-            userScoreSum += userScores[i];
-        }
+        //for (var i = 0; i < userScores.length; i++) {
+        //   userScoreSum += userScores[i];
+        //}
 
         for (var i = 0; i < friends.length; i++) {
-            var totalDifference = userScoreSum;
-            for (var j = 0; j < friends[i].Scores.length; j++) {
-                totalDifference = Math.abs(totalDifference - friends[i].Scores[j])
-            }
-
-            if (totalDifference < bestMatch.scoreDifference) {
-                bestMatch.name = freinds[i].name;
-                bestMatch.photo = freinds[i].photo;
-                bestMatch.scoreDifference = totalDifference;
+            //console.log(friends[i]);
+            totalDifference = 0;
+            for (var j = 0; j < friends[i].scores[j]; j++) {
+                totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
+                if (totalDifference <= bestMatch.scoreDifference) {
+                    bestMatch.name = friends[i].name;
+                    bestMatch.photo = friends[i].photo;
+                    bestMatch.scoreDifference = totalDifference;
+                }
             }
         }
 
